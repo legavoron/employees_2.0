27ccf5cd6c5f72657a29d2d29b8ba99fc303cbdd
Aleh Shchurko <alehshchurko@gmail.com>
2022-06-05 15:56:18 +0300

    add state of EmployeesItem

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,31 +1,70 @@
-import './employees-list-item.css'
+import './employees-list-item.css';
+import {Component} from 'react';
 
-const EmployeesListItem = () => {
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span    className="list-group-item-label" >
-                Aleh Shchurko
-            </span>
-            <input type="text" 
-                className="list-group-item-input" 
-                defaultValue='1000$'
-            />
-            <div className="d-flex justify-content-center align-items-center">
-                <button 
-                    type="button"
-                    className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            like: false
+        }
+    }
 
-                <button 
-                    type="button"
-                    className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+
+        const {name, salary} = this.props;
+        const {increase, like} = this.state;
+
+        let classes = 'list-group-item d-flex justify-content-between';
+
+        if (increase) {
+        classes += ' increase'
+        }
+
+        if (like) {
+            classes += ' like'
+        }
+
+        return (
+            <li className={classes}>
+                <span    className="list-group-item-label" 
+                    onClick={this.onLike}>
+                    {name}
+                </span>
+                <input type="text" 
+                    className="list-group-item-input" 
+                    defaultValue={salary + '$'}
+                />
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        type="button"
+                        className="btn-cookie btn-sm"
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button 
+                        type="button"
+                        className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,22 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css'
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeesListItem key={id} {...itemProps}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+
+            {/* <EmployeesListItem name="John Smith" salary={800}/>
+            <EmployeesListItem name="Endi Carrol" salary={3000}/>
+            <EmployeesListItem name="Aleh Shchurko" salary={5000}/> */}
+            {elements}
         </ul>
     )
 }
